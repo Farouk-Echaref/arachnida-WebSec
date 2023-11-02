0ab825519ceada2b9a1adf921bdf028fb341945a
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2023-11-02 03:16:14 +0100

    feat: class to store option & structure of the program using click

--- spider/main.py
+++ spider/main.py
@@ -1,13 +1,29 @@
 import click
 
+class Params:
+    def __init__(self, url="", method="unoDepth", depth=5, path="./data/"):
+        self.url = url
+        self.method = method
+        self.depth = depth
+        self.path = path
+    
+    #Verify (test method)
+    def __str__(self):
+        return f"URL: {self.url}, Method: {self.method}, Depth: {self.depth}"
+
+
+def start_scraping(r, p):
+    """A program for downloading files."""
+    click.echo(f"Recursive Download: {r}")
+    click.echo(f"Download Path: {p}")
+
 @click.command()
-@click.option('-count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+@click.option('-r', default=1, help='Recursive Download')
+@click.option('-p', prompt='Download Path')
+def main(r, p):
+    click.echo("Welcome to fechScraping:")
+    start_scraping(r, p)
+    
 
 if __name__ == '__main__':
-    hello()
+    main()
