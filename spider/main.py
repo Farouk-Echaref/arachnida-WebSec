5857c559f8bdfec1c878e155c55711f7b654017b
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2023-11-02 03:39:43 +0100

    feat: structure for handling option & combined options

--- spider/main.py
+++ spider/main.py
@@ -12,17 +12,44 @@ class Params:
         return f"URL: {self.url}, Method: {self.method}, Depth: {self.depth}"
 
 
-def start_scraping(r, p):
+def combined_options(combined):
+    if combined:
+        for c in combined:
+            if c == 'r':
+                #handle -r
+                click.echo("Recursive Download")
+            elif c == 'l':
+                #handle -l
+                click.echo("Recursive Download Depth")
+            elif c == 'p':
+                #handle -p
+                click.echo("Setting Download Path")
+            else:
+                click.echo("Unknown Option: {c}")
+                
+
+def start_scraping(r, l, p):
     """A program for downloading files."""
-    click.echo(f"Recursive Download: {r}")
-    click.echo(f"Download Path: {p}")
+    if r and l is not None:
+        # Both -r and -l were provided with their respective arguments.
+        click.echo(f"Recursive download with depth {l} and download path {p}.")
+    elif r:
+        # Only -r was provided without -l.
+        click.echo(f"Recursive download with default depth and download path {p}.")
+    else:
+        # No -r provided.
+        click.echo(f"Downloading without recursion to path {p}.")
+
 
 @click.command()
-@click.option('-r', default=1, help='Recursive Download')
-@click.option('-p', prompt='Download Path')
-def main(r, p):
+@click.option('-r', is_flag=True, help='Enable Recursive Download')
+@click.option('-l', type=int, help='Set Depth Of Recursive Download')
+@click.option('-p', type=str, prompt='Download Path', help='Set Download Path')
+@click.option('-c', '--combined', type=str, help='Combined Options')
+def main(r, l, p, combined):
     click.echo("Welcome to fechScraping:")
-    start_scraping(r, p)
+    combined_options(combined)
+    start_scraping(r, l, p)
     
 
 if __name__ == '__main__':
