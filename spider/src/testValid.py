f6601a341c924e4ff07c1349455ea607e4c1b009
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2023-11-06 21:14:45 +0100

    validating URLs test

--- spider/src/testValid.py
+++ spider/src/testValid.py
@@ -1,12 +1,10 @@
+#!/usr/bin/python3
 import validators
 
 
-validation = validators.url("http:/www.google.com")
+validation = validators.url("images.unsplash.com/photo-1696075619078-6cb640d329ec?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8")
 
 if validation:
-
-print("URL is valid")
-
+    print("URL is valid")
 else:
-
-print("URL is invalid")
+    print("URL is invalid")
