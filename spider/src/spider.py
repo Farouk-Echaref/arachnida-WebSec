ac4607dd6b415a7dd46d4139dc360aef702931fd
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2023-11-19 21:34:45 +0100

    feat: add recursive function to the structure

--- spider/src/spider.py
+++ spider/src/spider.py
@@ -3,13 +3,14 @@
 import os
 import click
 import requests
+from bs4 import BeautifulSoup
 from typing import List, Union, Optional
 from urllib.parse import urlparse, urljoin, ParseResult
 from urllib import robotparser
 
 USER_AGENT = "SpiderBot"
 EXT = [".jpg", ".jpeg", ".bmp", ".png", ".gif"]
-storeURLS: set = set()
+storeURLS: set[str] = set()
 
 def urlChecking(arg: List[Union[bool, int, str, str]]) -> None:
     result: ParseResult = urlparse(arg[3])
@@ -34,11 +35,11 @@ def mkdirSave(arg: List[Union[bool, int, str, str]]) -> None:
         raise Exception("Permission Denied")
     return
 
-def getContentFromUrl(arg: List[Union[bool, int, str, str]]) -> bytes:
-    pureUrl: str = urlparse(arg[3]).scheme + "://" + urlparse(arg[3]).netloc
+def getContentFromUrl(url: str) -> bytes:
+    pureUrl: str = urlparse(url).scheme + "://" + urlparse(url).netloc
     robotUrl = pureUrl + '/robots.txt'
     if(requests.get(robotUrl).status_code == 200):
-        pathToCheck = urlparse(arg[3]).path
+        pathToCheck = urlparse(url).path
         # class to parse robot file
         parseRobot = robotparser.RobotFileParser()
         parseRobot.set_url(robotUrl)
@@ -48,22 +49,45 @@ def getContentFromUrl(arg: List[Union[bool, int, str, str]]) -> bytes:
             raise Exception("Robots.txt forbids path: ", pathToCheck)
         elif (check == True):
             print("Robots.txt allows path: ", pathToCheck)
-        response = requests.get(arg[3])
+        response = requests.get(url)
         response.raise_for_status()
         return (response.content)
     return (666)
 
-def downloadImagesRecursively(arg: List[Union[bool, int, str, str]]) -> None:
+def retrieveNewLinks():
+    return
+
+def imagesDonwloader():
+    return
+
+def recursiveImageDownloader(arg: List[Union[bool, int, str, str]], current_level: int, incomingUrl: str) -> None:
+    # recursive base condition
+    if current_level >= arg[1]:
+        return
+    if incomingUrl in storeURLS:
+        return
+    #global set to store unique urls
+    storeURLS.add(incomingUrl)
+    try:
+        content: bytes = getContentFromUrl(incomingUrl)
+        if (content == 666):
+            raise Exception("Bad URL")
+        soup = BeautifulSoup(content, 'lxml')
+        print(soup)
+        imagesDonwloader()
+        if current_level + 1 < arg[1]:
+            newLinks: set[str] = retrieveNewLinks()
+            for link in newLinks:
+                recursiveImageDownloader(arg, current_level + 1, link)
+    except Exception as e:
+        print(f"{e}")
     return
 
 def startScraping(arg: List[Union[bool, int, str, str]]) -> None:
     try:
         urlChecking(arg)
         mkdirSave(arg)
-        # content: bytes = getContentFromUrl(arg)
-        # if (content == 666):
-        #     print("Error requesting the URL")
-        downloadImagesRecursively(arg)
+        recursiveImageDownloader(arg, 0, arg[3])
     except KeyboardInterrupt:
         # Some behavior for the signal
         # Exit status code for SIGINT
--- spider/src/testreq.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import requests
-
-url = "http://commons.wikimedia.org/robots.txt"
-res = requests.get(url)
-
-print(res)
