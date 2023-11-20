a1fdb2f243bd2b35bf5f7143c74a0df40243b063
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2023-11-20 03:14:14 +0100

    feat: forming full img links

--- spider/src/spider.py
+++ spider/src/spider.py
@@ -3,7 +3,7 @@
 import os
 import click
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet
 from typing import List, Union, Optional
 from urllib.parse import urlparse, urljoin, ParseResult
 from urllib import robotparser
@@ -11,19 +11,6 @@ from urllib import robotparser
 USER_AGENT = "SpiderBot"
 EXT = [".jpg", ".jpeg", ".bmp", ".png", ".gif"]
 storeURLS: set[str] = set()
-
-def urlChecking(arg: List[Union[bool, int, str, str]]) -> None:
-    result: ParseResult = urlparse(arg[3])
-    print(result)
-    if result.scheme == '':
-        arg[3] = "http://" + arg[3]
-        print(arg[3])
-        urlChecking(arg)
-    elif result.netloc == '':
-        raise Exception('No URL Netloc')
-    elif (result.scheme != "http" and result.scheme != "https") :
-        print(arg[3])
-        raise Exception('Invalid URL')
 
 def mkdirSave(arg: List[Union[bool, int, str, str]]) -> None:
     if (os.path.exists(arg[2]) == False):
@@ -35,21 +22,31 @@ def mkdirSave(arg: List[Union[bool, int, str, str]]) -> None:
         raise Exception("Permission Denied")
     return
 
+def urlChecking(arg: List[Union[bool, int, str, str]]) -> None:
+    result: ParseResult = urlparse(arg[3])
+    if result.scheme == '':
+        arg[3] = "http://" + arg[3]
+        urlChecking(arg)
+    elif result.netloc == '':
+        raise Exception('No URL Netloc')
+    elif (result.scheme != "http" and result.scheme != "https"):
+        raise Exception('Invalid URL')
+
 def getContentFromUrl(url: str) -> bytes:
     pureUrl: str = urlparse(url).scheme + "://" + urlparse(url).netloc
-    robotUrl = pureUrl + '/robots.txt'
+    robotUrl: str = pureUrl + '/robots.txt'
     if(requests.get(robotUrl).status_code == 200):
         pathToCheck = urlparse(url).path
         # class to parse robot file
         parseRobot = robotparser.RobotFileParser()
         parseRobot.set_url(robotUrl)
         parseRobot.read()
-        check = parseRobot.can_fetch(USER_AGENT, pathToCheck)
+        check: bool = parseRobot.can_fetch(USER_AGENT, pathToCheck)
         if (check == False):
             raise Exception("Robots.txt forbids path: ", pathToCheck)
         elif (check == True):
             print("Robots.txt allows path: ", pathToCheck)
-        response = requests.get(url)
+        response: response = requests.get(url)
         response.raise_for_status()
         return (response.content)
     return (666)
@@ -57,8 +54,31 @@ def getContentFromUrl(url: str) -> bytes:
 def retrieveNewLinks():
     return
 
-def imagesDonwloader():
+def getFullUrl(url: str, path: str):
+    parsedUrl = urlparse(path)
+
+    # If the path doesn't have a netloc (network location), 
+    # it is a relative path, and the full URL is resolved using urljoin.
+    if parsedUrl.netloc == '':
+        return urljoin(url, parsedUrl.path)
+    #If the path has a netloc but no scheme, it is assumed to be an HTTP link.
+    elif parsedUrl.scheme == '':
+        return 'http://' + parsedUrl.netloc + parsedUrl.path
+    #If the path has both a scheme and a netloc, it is considered a complete URL.
+    return parsedUrl.scheme + '://' + parsedUrl.netloc + parsedUrl.path
+
+def downloadOneImg(arg: List[Union[bool, int, str, str]], fullImgUrl: str) -> None:
     return
+
+def imagesDownloader(arg: List[Union[bool, int, str, str]], soup: BeautifulSoup, incomingUrl: str) -> None:
+    imgTags: ResultSet = soup.find_all('img')
+    for img in imgTags:
+        imgPath:  str = img.get('src')
+        if imgPath and os.path.splitext(imgPath)[1].lower() not in EXT:
+            continue
+        fullImgUrl: str = getFullUrl(incomingUrl, imgPath)
+        print(fullImgUrl)
+        downloadOneImg(arg, fullImgUrl)
 
 def recursiveImageDownloader(arg: List[Union[bool, int, str, str]], current_level: int, incomingUrl: str) -> None:
     # recursive base condition
@@ -72,16 +92,15 @@ def recursiveImageDownloader(arg: List[Union[bool, int, str, str]], current_leve
         content: bytes = getContentFromUrl(incomingUrl)
         if (content == 666):
             raise Exception("Bad URL")
-        soup = BeautifulSoup(content, 'lxml')
-        print(soup)
-        imagesDonwloader()
+        soup: BeautifulSoup = BeautifulSoup(content, 'lxml')
+        # print(soup)
+        imagesDownloader(arg, soup, incomingUrl)
         if current_level + 1 < arg[1]:
             newLinks: set[str] = retrieveNewLinks()
             for link in newLinks:
                 recursiveImageDownloader(arg, current_level + 1, link)
     except Exception as e:
         print(f"{e}")
-    return
 
 def startScraping(arg: List[Union[bool, int, str, str]]) -> None:
     try:
